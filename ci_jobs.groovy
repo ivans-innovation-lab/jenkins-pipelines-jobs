d7311259320e2a0ff1986fd04d17ac1342580121
Ivan Dugalic <idugalic@gmail.com>
2017-03-17 15:33:45 +0100

    Update ci_jobs.groovy

--- ci_jobs.groovy
+++ ci_jobs.groovy
@@ -13,7 +13,7 @@ for (repo in repos)
         credentialsId('git')
         includes("master feature/*")
         configure { git ->
-          git / 'sources' / 'data[0]' / 'jenkins.branch.BranchSource' / 'source' / 'extensions'  << 'hudson.plugins.git.extensions.impl.UserExclusion' {
+          git / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'extensions'  << 'hudson.plugins.git.extensions.impl.UserExclusion' {
             excludedUsers('idugalic')
           }       
         }
