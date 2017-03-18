1fdf8431f6c645fd829d6ba00ba463b47de526ac
Ivan Dugalic <idugalic@gmail.com>
2017-03-18 16:07:32 +0100

    Update ci_jobs.groovy

--- ci_jobs.groovy
+++ ci_jobs.groovy
@@ -14,7 +14,7 @@ for (repo in repos)
         includes("master feature/*")
         configure { project -> 
             def branches = (project / 'sources' / 'data' /'jenkins.branch.BranchSource')
-            branches.getTest()
+            echo branches.class()
               
          }
       }
