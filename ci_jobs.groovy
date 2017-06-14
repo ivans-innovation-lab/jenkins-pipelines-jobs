6a65dd71a00e465874e8c5c825a3d6fb2253c10e
Ivan Dugalic <idugalic@gmail.com>
2017-06-14 21:33:29 +0200

    Create ci_jobs.groovy

--- ci_jobs.groovy
+++ ci_jobs.groovy
@@ -4,7 +4,7 @@ import javax.xml.parsers.*;
 import java.io.IOException;
 
 // define the repos we want to build on CI
-def repos = [ 'my-company-common','my-company-project-materialized-view','my-company-project-domain','my-company-blog-materialized-view','my-company-blog-domain','my-company-monolith' ]
+def repos = [ 'my-company-common','my-company-support','my-company-project-materialized-view','my-company-project-domain','my-company-blog-materialized-view','my-company-blog-domain','my-company-monolith','my-company-blog-domain-microservice','my-company-blog-materialized-view-microservice','my-company-project-domain-microservice','my-company-project-materialized-view-microservice','my-company-api-gateway-backingservice','my-company-registry-backingservice','my-company-configuration-backingservice','my-company-adminserver-backingservice' ]
 
 // create a multibranch pipeline job for each of the repos
 for (repo in repos)
